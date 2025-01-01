c326292c02d5dceb39a5c5a47f31dbcdf0836236
James Pelster <jpmac26@gmail.com>
2025-01-01 12:00:14 -0800

    Enable new Earth gravity model & RTCC support for it

--- Orbitersdk/samples/ProjectApollo/src_rtccmfd/EnckeIntegrator.cpp
+++ Orbitersdk/samples/ProjectApollo/src_rtccmfd/EnckeIntegrator.cpp
@@ -691,11 +691,11 @@ void EnckeFreeFlightIntegrator::SetBodyParameters(int p)
 		rect2 = 0.75*OrbMech::power(2.0, 2.0)*100.0;
 		P = BODY_EARTH;
 		GMD = 4;
-		GMO = 0; //4 to use the full tesseral data
+		GMO = 4; //4 to use the full tesseral data
 		ZONAL[0] = 0.0; ZONAL[1] = OrbMech::J2_Earth; ZONAL[2] = OrbMech::J3_Earth; ZONAL[3] = OrbMech::J4_Earth;
 		//Use this when Orbiter simulates it
-		//C[0] = -1.1619e-9; C[1] =  1.5654e-6; C[2] = 2.1625e-6; C[3] =  3.18750e-7; C[4] = 9.7078e-8; C[5] = -5.1257e-7; C[6] = 7.739e-8; C[7] =  5.7700e-8; C[8] = -3.4567e-9;
-		//S[0] = -4.1312e-9; S[1] = -8.9613e-7; S[2] = 2.6809e-7; S[3] = -2.15567e-8; S[4] = 1.9885e-7; S[5] = -4.4095e-7; S[6] = 1.497e-7; S[7] = -1.2389e-8; S[8] =  6.4464e-9;
+		C[0] = -1.1619e-9; C[1] =  1.5654e-6; C[2] = 2.1625e-6; C[3] =  3.18750e-7; C[4] = 9.7078e-8; C[5] = -5.1257e-7; C[6] = 7.739e-8; C[7] =  5.7700e-8; C[8] = -3.4567e-9;
+		S[0] = -4.1312e-9; S[1] = -8.9613e-7; S[2] = 2.6809e-7; S[3] = -2.15567e-8; S[4] = 1.9885e-7; S[5] = -4.4095e-7; S[6] = 1.497e-7; S[7] = -1.2389e-8; S[8] =  6.4464e-9;
 	}
 	else
 	{
